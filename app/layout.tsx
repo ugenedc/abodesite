9b92c0bd6369d75684b2a611c6d552d3e6af9e91
ugenedc <ugenedc@gmail.com>
2025-06-14 00:01:06 +1000

    fix: upgrade mapbox-gl-js to v3.4.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link href="https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css" rel="stylesheet" />
+        <link href="https://api.mapbox.com/mapbox-gl-js/v3.4.0/mapbox-gl.css" rel="stylesheet" />
       </head>
       <body>
         <SiteLayout>{children}</SiteLayout>
-        <Script src="https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.js" strategy="beforeInteractive" />
+        <Script src="https://api.mapbox.com/mapbox-gl-js/v3.4.0/mapbox-gl.js" strategy="beforeInteractive" />
       </body>
     </html>
   )
