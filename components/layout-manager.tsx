961e744a6fdbef1db4e63337106ac4bf4d5de2c2
ugenedc <ugenedc@gmail.com>
2025-06-14 01:35:42 +1000

    fix: isolate map-debug-2 page layout

--- components/layout-manager.tsx
+++ components/layout-manager.tsx
@@ -5,7 +5,7 @@ import SiteLayout from "./site-layout"
 
 export default function LayoutManager({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
-  const isDebugPage = pathname === "/map-debug"
+  const isDebugPage = pathname === "/map-debug" || pathname === "/map-debug-2"
 
   if (isDebugPage) {
     return <>{children}</>
