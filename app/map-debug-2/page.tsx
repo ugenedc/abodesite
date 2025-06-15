c036f2e34addc0e038d9cab14189c37ef26f433a
ugenedc <ugenedc@gmail.com>
2025-06-15 20:23:19 +1000

    fix: Disable user interaction on debug map

--- app/map-debug-2/page.tsx
+++ app/map-debug-2/page.tsx
@@ -6,7 +6,7 @@ export default function MapDebugPage() {
   return (
     <div className="bg-gradient-to-br from-pink-400 via-purple-400 to-orange-300" style={{ position: "fixed", top: 0, left: 0, right: 0, bottom: 0 }}>
       <MapCanvas
-        interactive={true}
+        interactive={false}
         style="mapbox://styles/mapbox/light-v11"
         className="mix-blend-overlay"
         animate={true}
